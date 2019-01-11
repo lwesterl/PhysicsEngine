55141748b94234a6abf33d6026e6ef7fe46ca96e
lwesterl <lauri.v.westerholm@gmail.com>
2019-01-11 21:10:01 +0200

    add namespace pe

--- utils/Vector2.cpp
+++ utils/Vector2.cpp
@@ -6,54 +6,58 @@
 
 #include "Vector2.hpp"
 
+namespace pe {
 
-/*  Constructors */
-template<typename T>
-Vector2<T>::Vector2(T x, T y): x(x), y(y) {}
 
-template<typename T>
-Vector2<T>::Vector2(): x(static_cast<T> (0)), y(static_cast<T> (0)) {}
+  /*  Constructors */
+  template<typename T>
+  Vector2<T>::Vector2(T x, T y): x(x), y(y) {}
 
-template<typename T>
-Vector2<T>::Vector2(const Vector2<T>& vector2): x(vector2.x), y(vector2.y) {}
+  template<typename T>
+  Vector2<T>::Vector2(): x(static_cast<T> (0)), y(static_cast<T> (0)) {}
 
-template<typename T>
-T Vector2<T>::getX() {
-  return x;
-}
+  template<typename T>
+  Vector2<T>::Vector2(const Vector2<T>& vector2): x(vector2.x), y(vector2.y) {}
 
-template<typename T>
-T Vector2<T>::getY() {
-  return y;
-}
+  template<typename T>
+  T Vector2<T>::getX() {
+    return x;
+  }
 
-template<typename T>
-void Vector2<T>::update(T x, T y) {
-  this->x = x;
-  this->y = y;
-}
+  template<typename T>
+  T Vector2<T>::getY() {
+    return y;
+  }
 
-template<typename T>
-Vector2<T>& Vector2<T>::operator=(const Vector2<T>& vector2) {
-  this->x = vector2.x;
-  this->y = vector2.y;
-  return *this;
-}
+  template<typename T>
+  void Vector2<T>::update(T x, T y) {
+    this->x = x;
+    this->y = y;
+  }
 
-template<typename T>
-bool Vector2<T>::operator==(const Vector2& vector) {
-  return ( (x == vector.x) && (y == vector.y) );
-}
+  template<typename T>
+  Vector2<T>& Vector2<T>::operator=(const Vector2<T>& vector2) {
+    this->x = vector2.x;
+    this->y = vector2.y;
+    return *this;
+  }
 
-template<typename T>
-Vector2<T>& Vector2<T>::operator*=(T mult) {
-  this->x *= mult;
-  this->y *= mult;
-  return *this;
-}
+  template<typename T>
+  bool Vector2<T>::operator==(const Vector2& vector) {
+    return ( (x == vector.x) && (y == vector.y) );
+  }
 
-/*  Instantiate needed templates */
-template class Vector2<int>;
-template class Vector2<float>;
-template class Vector2<unsigned>;
-template class Vector2<double>;
+  template<typename T>
+  Vector2<T>& Vector2<T>::operator*=(T mult) {
+    this->x *= mult;
+    this->y *= mult;
+    return *this;
+  }
+
+  /*  Instantiate needed templates */
+  template class Vector2<int>;
+  template class Vector2<float>;
+  template class Vector2<unsigned>;
+  template class Vector2<double>;
+
+} // end of namespace pe
--- utils/Vector2.hpp
+++ utils/Vector2.hpp
@@ -10,105 +10,113 @@
 #include <ostream>
 
 /**
-  *   @class Vector2
-  *   @brief Template class for 2D-vectors
+  *   @namespace pe
+  *   @remark Stands for PhysicsEngine
   */
+namespace pe {
 
-template <typename T>
-class Vector2
-{
-  public:
-    /**
-      *   @brief Standard constructor
-      *   @param x x coordinate
-      *   @param y y coordinate
-      */
-    Vector2(T x, T y);
+  /**
+    *   @class Vector2
+    *   @brief Template class for 2D-vectors
+    */
 
-    /**
-      *   @brief Empty constructor
-      *   @remark sets x and y to 0
-      */
-    Vector2();
+  template <typename T>
+  class Vector2
+  {
+    public:
+      /**
+        *   @brief Standard constructor
+        *   @param x x coordinate
+        *   @param y y coordinate
+        */
+      Vector2(T x, T y);
 
-    /**
-      *   @brief Copy constructor
-      *   @param vector2 Vector2 instance to be copied
-      */
-    Vector2(const Vector2<T>& vector2);
+      /**
+        *   @brief Empty constructor
+        *   @remark sets x and y to 0
+        */
+      Vector2();
 
-    /**
-      *   @brief Get x value
-      *   @return x
-      */
-    T getX();
+      /**
+        *   @brief Copy constructor
+        *   @param vector2 Vector2 instance to be copied
+        */
+      Vector2(const Vector2<T>& vector2);
 
-    /**
-      *   @brief Get y value
-      *   @return y
-      */
-    T getY();
+      /**
+        *   @brief Get x value
+        *   @return x
+        */
+      T getX();
 
-    /**
-      *   @brief Update x and y values
-      *   @param x new x value
-      *   @param y new y value
-      */
-    void update(T x, T y);
+      /**
+        *   @brief Get y value
+        *   @return y
+        */
+      T getY();
 
-    /**
-      *   @brief Assignment operator
-      *   @param vector2 Reference instance
-      */
-    Vector2<T>& operator=(const Vector2<T>& vector2);
+      /**
+        *   @brief Update x and y values
+        *   @param x new x value
+        *   @param y new y value
+        */
+      void update(T x, T y);
 
-    /**
-      *   @brief Equality implementation
-      *   @param vector Vector2 which is used as comparison
-      *   @return true if x and y equal, otherwise false
-      */
-    bool operator==(const Vector2& vector);
+      /**
+        *   @brief Assignment operator
+        *   @param vector2 Reference instance
+        */
+      Vector2<T>& operator=(const Vector2<T>& vector2);
 
-    Vector2<T>& operator*=(T mult);
+      /**
+        *   @brief Equality implementation
+        *   @param vector Vector2 which is used as comparison
+        *   @return true if x and y equal, otherwise false
+        */
+      bool operator==(const Vector2& vector);
 
-    /**
-      *   @brief multiplication overload
-      *   @param vect1 the first passed Vector2
-      *   @param vect2 the second passed Vector2
-      *   @return Vector2 which is original vectors multiplied
-      */
-    friend Vector2<T> operator*(const Vector2<T>& vect1, const Vector2<T>& vect2) {
-      return Vector2<T>(vect1.x * vect2.x, vect1.y * vect2.y);
-    }
+      Vector2<T>& operator*=(T mult);
 
-    /**
-      *   @brief addition overload
-      *   @param vect1 the first passed Vector2
-      *   @param vect2 the second passed Vector2
-      *   @return Vector2 which is sum of the original vectors
-      */
-    friend Vector2<T> operator+(const Vector2<T>& vect1, const Vector2<T>& vect2) {
-      return Vector2<T>(vect1.x + vect2.x, vect1.y + vect2.y);
-    }
+      /**
+        *   @brief multiplication overload
+        *   @param vect1 the first passed Vector2
+        *   @param vect2 the second passed Vector2
+        *   @return Vector2 which is original vectors multiplied
+        */
+      friend Vector2<T> operator*(const Vector2<T>& vect1, const Vector2<T>& vect2) {
+        return Vector2<T>(vect1.x * vect2.x, vect1.y * vect2.y);
+      }
 
-    /**
-      *   @brief <<-operator overload
-      *   @param os output ostream
-      *   @param vector2 Constant Vector2 instance
-      */
-    friend std::ostream& operator<<(std::ostream &os, const Vector2<T> &vector2) {
-      os << "X: " << vector2.x << ", Y: " << vector2.y << std::endl;
-      return os;
-    }
+      /**
+        *   @brief addition overload
+        *   @param vect1 the first passed Vector2
+        *   @param vect2 the second passed Vector2
+        *   @return Vector2 which is sum of the original vectors
+        */
+      friend Vector2<T> operator+(const Vector2<T>& vect1, const Vector2<T>& vect2) {
+        return Vector2<T>(vect1.x + vect2.x, vect1.y + vect2.y);
+      }
+
+      /**
+        *   @brief <<-operator overload
+        *   @param os output ostream
+        *   @param vector2 Constant Vector2 instance
+        */
+      friend std::ostream& operator<<(std::ostream &os, const Vector2<T> &vector2) {
+        os << "X: " << vector2.x << ", Y: " << vector2.y << std::endl;
+        return os;
+      }
 
 
-  private:
-    T x;
-    T y;
-};
+    private:
+      T x;
+      T y;
+  };
 
-/*  Define shorter keywords */
-#define Vector2f Vector2<float> /**< Macro for float Vector2 */
-#define Vector2i Vector2<int> /**< Macro for int Vector2 */
-#define Vector2d Vector2<double> /**< Macro for double Vector2 */
-#define Vector2u Vector2<unsigned> /**< Macro for unsigned Vector2 */
+  /*  Define shorter keywords */
+  #define Vector2f Vector2<float> /**< Macro for float Vector2 */
+  #define Vector2i Vector2<int> /**< Macro for int Vector2 */
+  #define Vector2d Vector2<double> /**< Macro for double Vector2 */
+  #define Vector2u Vector2<unsigned> /**< Macro for unsigned Vector2 */
+
+} // end of namespace pe
