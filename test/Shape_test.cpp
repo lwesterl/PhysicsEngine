f5f01239e4b4dd26a1d41d4552d659749cb3b276
lwesterl <lauri.v.westerholm@gmail.com>
2019-01-12 19:12:41 +0200

    add test main for Shape class

---
+++ test/Shape_test.cpp
@@ -0,0 +1,17 @@
+/**
+  *   @file Shape_test.cpp
+  *   @author Lauri Westerholm
+  *   @brief Contains test main for Shape
+  */
+
+#include "../include/Shape.hpp"
+#include <assert.h>
+
+/**
+  *   @brief Test main for Shape
+  */
+
+int main() {
+  pe::Shape shape;
+  return 0;
+}
