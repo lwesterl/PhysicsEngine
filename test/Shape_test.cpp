5d49ce9c399d02517ffe4d83f6bf9f778b1d9442
lwesterl <lauri.v.westerholm@gmail.com>
2019-01-13 17:42:02 +0200

    add tests for new implementations in Shape

--- test/Shape_test.cpp
+++ test/Shape_test.cpp
@@ -5,13 +5,50 @@
   */
 
 #include "../include/Shape.hpp"
-#include <assert.h>
+#include <iostream>
+#include <cassert>
 
 /**
   *   @brief Test main for Shape
   */
 
 int main() {
+
+  std::cout << "Shape constructor test" << std::endl;
   pe::Shape shape;
+  assert(shape.getCenter() == pe::Vector2f(0.f, 0.f));
+  shape = pe::Shape(10.f, 10.f);
+  pe::Vector2f center = shape.getCenter();
+  std::cout << "Center: " << center;
+  assert(center == pe::Vector2f(5.f, 5.f));
+  pe::Shape shape2 = shape;
+  assert(shape2.getCenter() == shape.getCenter());
+  std::cout << "Test succesful" << std::endl;
+
+  std::cout << std::endl << "getFrame test" << std::endl;
+  std::deque<pe::Vector2f> frame = shape2.getFrame();
+  for (auto item : frame) {
+    std::cout << item;
+  }
+  std::cout << "Test succesful" << std::endl;
+
+  std::cout << std::endl << "getMin and getMax test" << std::endl;
+  pe::Shape test1;
+  assert(test1.getMin() == pe::Vector2f(0.f, 0.f));
+  assert(test1.getMax() == pe::Vector2f(0.f, 0.f));
+  test1 = pe::Shape(30.f, 25.f);
+  assert(test1.getMin() == pe::Vector2f(0.f, 0.f));
+  assert(test1.getMax() == pe::Vector2f(30.f, 25.f));
+  pe::Shape test2 = pe::Shape(100.f, -100.f); // should take abs from negative values
+  assert(test2.getMin() == pe::Vector2f(0.f, 0.f));
+  frame = test2.getFrame();
+  for (auto item : frame) {
+    std::cout << item;
+  }
+  assert(test2.getMax() == pe::Vector2f(100.f, 100.f));
+  std::cout << "Test succesful" << std::endl;
+
+  std::cout << std::endl << "All test succesfully completed" << std::endl;
+
   return 0;
 }
