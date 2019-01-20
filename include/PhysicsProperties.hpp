4ac0b1c4d97b0a30df8249437b18fbc5ea7155e0
lwesterl <lauri.v.westerholm@gmail.com>
2019-01-20 18:33:04 +0200

    remove force variable

--- include/PhysicsProperties.hpp
+++ include/PhysicsProperties.hpp
@@ -52,10 +52,9 @@ namespace pe {
 
         Vector2f velocity; /**< Velocity (x, y) */
         Vector2f acceloration; /**< Acceloration (x, y) */
-        Vector2f force; /**< Force (x, y) */
         Vector2f position; /**< Actual position of the object in PhysicsWorld, normally relating to the center of mass of the Shape */
         Vector2f origin_transform; /**< Transform from the object center of mass to user defined point */
-        float angle; /**< Rotation angle in radians */ 
+        float angle; /**< Rotation angle in radians */
         float density; /**< 2D density of the object */
         float elasticity; /**< Bounciness of the object, should be >= 0 */
         float inverse_mass; /**< Inverse of the object mass */
