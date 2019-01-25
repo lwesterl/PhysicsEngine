5c41f17af5e779263b8ea598920e7349b723b7a0
lwesterl <lauri.v.westerholm@gmail.com>
2019-01-25 21:58:46 +0200

    add make to the script

--- test/TestAll.sh
+++ test/TestAll.sh
@@ -1,8 +1,17 @@
 #! /bin/bash
+# author Lauri Westerholm
+# a small bash script which makes and runs all tests
+# exits on failure
 
+make
+if [ $? != 0 ]; then
+  printf "\nFail: make\n"
+  exit
+fi
+printf "\nInfo: make finished\n\n"
 Tests=(*.exe)
 for test in "${Tests[@]}"; do
-  echo "Running $test"
+  printf "\n\nRunning $test\n"
   ./$test
   if [ $? != 0 ]; then
     printf "\n$test failed\n"
