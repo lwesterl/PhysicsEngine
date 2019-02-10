736da5b10582c90ee407bf5812f4d37eed4f2683
lwesterl <lauri.v.westerholm@gmail.com>
2019-02-10 14:33:56 +0200

    add valgrind tests

--- test/TestAll.sh
+++ test/TestAll.sh
@@ -3,6 +3,10 @@
 # a small bash script which makes and runs all tests
 # exits on failure
 
+# runs tests by ./TestAll
+# runs memory leak tests by ./TestAll valgrind
+
+
 make
 if [ $? != 0 ]; then
   printf "\nFail: make\n"
@@ -12,7 +16,11 @@ printf "\nInfo: make finished\n\n"
 Tests=(*.exe)
 for test in "${Tests[@]}"; do
   printf "\n\nRunning $test\n"
-  ./$test
+  if [ $# == 1 ] && [ $1 == "valgrind" ]; then
+    valgrind --leak-check=yes ./$test
+  else
+    ./$test
+  fi
   if [ $? != 0 ]; then
     printf "\n$test failed\n"
     exit
