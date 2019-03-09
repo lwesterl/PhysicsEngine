118b6ccd931f57f14ca5bc01650f479773aa1270
lwesterl <lauri.v.westerholm@gmail.com>
2019-03-09 15:29:45 +0200

    add initials for demo.cpp containing demo main

---
+++ demo/demo.cpp
@@ -0,0 +1,38 @@
+/**
+  *   @file demo.cpp
+  *   @author Lauri Westerholm
+  *   @brief Demo/test for PhysicsEngine
+  *   @details Contains the demo main and other source code
+  */
+
+#include "DemoWorld.hpp"
+
+int main() {
+
+  // create the main window
+  sf::RenderWindow window(sf::VideoMode(800, 600), "PhysicsEngine Demo");
+
+  // create DemoWorld
+  DemoWorld demoWorld(window);
+  demoWorld.initWorld();
+
+  while(window.isOpen()) {
+    sf::Event event;
+    while(window.pollEvent(event)) {
+      if (demoWorld.handleEvent(event) < 0) {
+        // quit
+        return 0;
+      }
+    }
+    // clear all draw objects
+    window.clear();
+    // update demoWorld and draw it
+    demoWorld.update();
+    demoWorld.draw();
+    // show updated window
+    window.display();
+
+  }
+
+  return 0;
+}
