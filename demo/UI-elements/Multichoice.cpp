90c198897cd7161a27c2aa75c2997b0ea4e27b39
lwesterl <lauri.v.westerholm@gmail.com>
2019-03-31 01:30:14 +0200

    Partial implementation for Multichoice

---
+++ demo/UI-elements/Multichoice.cpp
@@ -0,0 +1,110 @@
+/**   @file Multichoice.cpp
+  *   @author Lauri Westerholm
+  *   @brief Contains source for Multichoice
+  */
+
+
+#include "Multichoice.hpp"
+
+namespace UI {
+
+  /* class Multichoice */
+
+  // class member initialization
+  unsigned Multichoice::FontSize = 20;
+  float Multichoice::HorizontalDistance = 50.f;
+  float Multichoice::VerticalDistance = 80.f;
+  float Multichoice::TitleDistance = 30.f;
+  float Multichoice::FrameSpace = 5.f;
+
+  // empty constructor
+  Multichoice::Multichoice(): value(0), interval(0) {}
+
+  // constructor
+  Multichoice::Multichoice(sf::String title, float x, float y, float width, float height):
+  value(0), interval(0) {
+    // if font loading fails, none of the text are visible
+    font.loadFromFile(FontPath);
+
+    this->title.setFont(font);
+    this->title.setCharacterSize(Multichoice::FontSize);
+    this->title.setString(title);
+    limits.maxText.setFont(font);
+    limits.maxText.setCharacterSize(Multichoice::FontSize);
+    limits.minText.setFont(font);
+    limits.minText.setCharacterSize(Multichoice::FontSize);
+    valueText.setFont(font);
+    valueText.setCharacterSize(Multichoice::FontSize);
+
+    // set correct frame width and height
+    outerFrame.setSize(sf::Vector2f(width, height));
+    innerFrame.setSize(sf::Vector2f(width - 2 * Multichoice::FrameSpace, height));
+
+    // set colors
+    outerFrame.setFillColor(sf::Color::Blue);
+    innerFrame.setFillColor(sf::Color::White);
+
+    // set correct positions
+    setPosition(x, y);
+  }
+
+  // set all scale related values
+  void Multichoice::setScale(int32_t initial_value, int32_t max, int32_t min, int32_t interval) {
+    value = initial_value;
+    this->interval = interval;
+    limits.max = max;
+    limits.min = min;
+    // convert to c-style string and then to sf::String (int32 fits in 11 chars)
+    char str_max[12], str_min[12];
+    snprintf(str_max, 12, "%d", limits.max);
+    snprintf(str_min, 12, "%d", limits.min);
+    limits.maxText.setString(str_max);
+    limits.minText.setString(str_min);
+    UpdateValueText();
+  }
+
+  // set all positions: title, frames, texts, buttons
+  void Multichoice::setPosition(float x, float y) {
+    title.setPosition(x, y);
+    // font is monospace, Courier (following is an approximation)
+    float title_width = static_cast<float>(Multichoice::FontSize)/2.f * title.getString().getSize();
+    float adjust = (title_width - outerFrame.getSize().x) / 2.f;
+    // set centered positions for the frames
+    outerFrame.setPosition(x + adjust, y + Multichoice::TitleDistance);
+    float difference = outerFrame.getSize().y - innerFrame.getSize().y;
+    innerFrame.setPosition(x + adjust + Multichoice::FrameSpace, y + Multichoice::TitleDistance + difference);
+  }
+
+  // update valueText, private method
+  void Multichoice::UpdateValueText() {
+    char str_value[12];
+    snprintf(str_value, 12, "%d", value);
+    valueText.setString(str_value);
+  }
+
+  // update innerFrame height based on value, private method
+  void Multichoice::UpdateInnerFrame() {
+    if (value) {
+      float value_multp = static_cast<float> (limits.max) - static_cast<float> (value);
+      // set new height for innerFrame
+      const sf::Vector2f& outer_size = outerFrame.getSize();
+      innerFrame.setSize(sf::Vector2f(outer_size.x - 2 * Multichoice::FrameSpace, outer_size.y * value_multp));
+      // update also position accordingly
+      innerFrame.setPosition(innerFrame.getPosition().x, outerFrame.getPosition().y + (outerFrame.getSize().y + innerFrame.getSize().y));
+    }
+  }
+
+  // draw frames, texts, buttons
+  void Multichoice::draw(sf::RenderTarget& target, sf::RenderStates states) const {
+    if (enabled) {
+      target.draw(outerFrame, states);
+      target.draw(innerFrame, states);
+      target.draw(title, states);
+      target.draw(limits.minText);
+      target.draw(limits.maxText);
+      target.draw(valueText);
+    }
+  }
+
+
+} // end of namespace UI
--- demo/UI-elements/Multichoice.hpp
+++ demo/UI-elements/Multichoice.hpp
@@ -1,40 +1,133 @@
-/**   @file Multichoice.hpp
+/**
+  *   @file Multichoice.hpp
   *   @author Lauri Westerholm
   *   @brief Multichoice for GameEngine demo, UI element
   */
 
 #pragma once
+#include "Switch.hpp" // contains font path macro
 #include <SFML/Graphics.hpp>
+#include <cstdlib>
+#include <cstdint>
 
-/**   @namespace UI
+
+/**
+  *   @namespace UI
   *   @brief namespace for UI elements used in PhysicsEngine demo
   */
 namespace UI {
 
-  /**   @class Multichoice
-    *   @brief Multichoice switch for demo
+  /**
+    *   @struct ValueLimits
+    *   @brief Stores limits and describes those using sf::Texts
     */
-  class Multichoice: sf::Drawable {
+  struct ValueLimits {
+    int32_t max = 0;
+    int32_t min = 0;
+    sf::Text maxText;
+    sf::Text minText;
+  };
+
+  /**
+    *   @class Multichoice
+    *   @brief Multichoice switch for demo
+    *   @details This has a basic layout of two-part central frame that demostrates
+    *   the current state (blue-white color theme). Aside the frame there is two
+    *   Buttons: increase and decrease, mouse activated. Title is on top of the
+    *   frame and it can be user defined.
+    */
+  class Multichoice: public sf::Drawable {
 
     public:
+      static unsigned FontSize; /**< font size used */
+      static float HorizontalDistance; /**< distance between main frame and buttons */
+      static float VerticalDistance; /**< vertical distance between buttons */
+      static float TitleDistance; /**< vertical distance between title and frames */
+      static float FrameSpace; /**< how much smaller innnerFrame is on the both sides */
 
-      /**   @brief Empty constructo
-        *   @details creates a basic blue-green switch at position 0.f
+      /**
+        *   @brief Empty constructor
         */
         Multichoice();
 
-      /**   @brief Constructor
+      /**
+        *   @brief Deconstructor
+        */
+        virtual ~Multichoice() {}
+
+      /**
+        *   @brief Constructor
         *   @param x x coordinate position
         *   @param y y coordinate position
-        *   @param intial_value starting value
+        *   @param width Multichoice frame width
+        *   @param height Multichoice frame height
+        *   @remark Positions are set based on title left corner
         */
-        Multichoice(float x, float y, int starting value);
+        Multichoice(sf::String title, float x, float y, float width, float height);
+
+      /**
+        *   @brief Set scale for Multichoice
+        *   @param initial_value initial value
+        *   @param max maximum value
+        *   @param min minumum value
+        *   @param interval how much one increase or decrease affects value
+        */
+        void setScale(int32_t initial_value, int32_t max, int32_t min, int32_t interval);
+
+      /**
+        *   @brief Set position
+        *   @param x x coordinate, title left corner
+        *   @param y y coordinate, title upper corner
+        */
+        void setPosition(float x, float y);
+
+      /**
+        *   @brief Draw Multichoice
+        *   @details Switch inherits from sf::Drawable
+        *   @param target Rendertarger where Switch is drawn to
+        *   @param states sf::Renderstates
+        */
+        virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+
+        bool tryToggle(float x, float y);
+
+        /**
+          *   @brief Get value
+          *   @return value
+          */
+        inline int32_t getValue() const {
+          return value;
+        }
+
+        /**
+          *   @brief Set enabled
+          *   @param enable true -> drawn and buttons can be clicked
+          */
+        inline void setEnabled(bool enable) {
+          enabled = enable;
+        }
 
     private:
 
-      int value;
+      /**
+        *   @brief Update valueText after value has been changed
+        */
+      void UpdateValueText();
+
+      /**
+        *   @brief Update innerFrame height when value (or scale) is changed
+        */
+      void UpdateInnerFrame();
+
+      bool enabled;
+      int32_t value;
+      int32_t interval;
+      struct ValueLimits limits;
       sf::RectangleShape outerFrame;
       sf::RectangleShape innerFrame;
+      sf::Text title;
+      sf::Text valueText;
+      sf::Font font;
 
   };
 
